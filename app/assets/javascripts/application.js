7496852c8312d8421ed778fc7eebb5ef576cd097
vagrant <vagrant@localhost.localdomain>
2020-09-04 07:25:39 +0000

    calender

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,10 @@
 //= require_tree .
 //= require jquery
 //= require moment
+//= require moment/ja.js
 //= require fullcalendar
+//= require fullcalendar/lang/ja
 
-$(function () {
-  function eventCalendar() {
-    return $('#calendar').fullCalendar({});
-  };
-  function clearCalendar() {
-    $('#calendar').html('');
-  };
+$(document).on("turbolinks:load", function() {
+  $("#calendar").fullCalendar({});
 });
-
-$(document).on('turbolinks:load', function () {
-  eventCalendar();
-});
-$(document).on('turbolinks:before-cache', clearCalendar);
